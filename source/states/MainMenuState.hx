50f2a283459f0ad085f87999b3d864dfd10ef24c
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-10-02 15:09:51 +0800

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -273,6 +273,8 @@ class MainMenuState extends MusicBeatState
 						FlxTween.tween(myLittleFinger.scale, {x: 0.3, y: 0.3}, 1.6, {ease: FlxEase.expoIn, onComplete: (twn:FlxTween)->
 						{
 							PlayState.SONG = Song.loadFromJson('cycles', 'cycles');
+							Paths.inst('cycles');
+							Paths.voices('cycles');
 							PlayState.isStoryMode = false;
 							PlayState.storyDifficulty = 1;
 

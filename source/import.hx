371bc6193ead8bece7cf522e986d32a123af5af9
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-10-02 13:55:12 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -44,7 +44,7 @@ import flxanimate.*;
 //import mobile.flixel.FlxVirtualPad;
 //import mobile.flixel.input.FlxMobileInputID;
 import mobile.backend.SUtil;
-//import mobile.backend.MobileScaleMode;
+import mobile.backend.MobileScaleMode;
 
 //Android
 #if android

a7b759aec4922041241ac2bb1993995b2b3a30ca
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-10-02 21:48:19 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -40,9 +40,9 @@ import flxanimate.*;
 #end
 
 //Mobile Controls
-//import mobile.flixel.FlxHitbox;
-//import mobile.flixel.FlxVirtualPad;
-//import mobile.flixel.input.FlxMobileInputID;
+import mobile.flixel.FlxHitbox;
+import mobile.flixel.FlxVirtualPad;
+import mobile.flixel.input.FlxMobileInputID;
 import mobile.backend.SUtil;
 import mobile.backend.MobileScaleMode;
 
